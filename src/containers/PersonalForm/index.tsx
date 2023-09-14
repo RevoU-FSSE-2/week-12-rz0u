634b714dea29fd5141bf43ebee85defb17f2ee22
rz0u <rayhan.zou98@gmail.com>
2023-09-14 16:42:14 +0700

    personal form

--- src/containers/PersonalForm/index.tsx
+++ src/containers/PersonalForm/index.tsx
@@ -1,5 +1,53 @@
-const PersonalForm = () => {
-  return <></>;
-};
+import { Formik, Field, ErrorMessage, FormikHelpers } from "formik";
+import { Form, Input, Button } from "antd";
+
+interface PersonalValues {
+  fullName: string;
+  emailAddress: string;
+  birthDate: string;
+}
+
+function PersonalForm() {
+  return (
+    <Formik
+      initialValues={{
+        fullName: "",
+        emailAddress: "",
+        birthDate: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(
+        values: PersonalValues,
+        { setSubmitting }: FormikHelpers<PersonalValues>
+      ) => {
+        console.log("Personal Form Values:", values);
+        setSubmitting(false);
+      }}
+    >
+      {() => (
+        <Form>
+          <div>
+            <label htmlFor="fullName">Full Name</label>
+            <Field type="text" name="fullName" as={Input} />
+            <ErrorMessage name="fullName" component="div" />
+          </div>
+          <div>
+            <label htmlFor="emailAddress">Email Address</label>
+            <Field type="email" name="emailAddress" as={Input} />
+            <ErrorMessage name="emailAddress" component="div" />
+          </div>
+          <div>
+            <label htmlFor="birthDate">Date of Birth</label>
+            <Field type="date" name="birthDate" as={Input} />
+            <ErrorMessage name="birthDate" component="div" />
+          </div>
+          <Button htmlType="submit" type="primary">
+            Next
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
 
 export default PersonalForm;
